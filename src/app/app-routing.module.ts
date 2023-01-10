9102561995c52531dea97c2ac3090b38b997053e
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-10 11:28:08 -0300

    feat: add Routes and routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'sobre',
+    component: AboutMeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
